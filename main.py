084f18b08e63481df39db1fcb7b82d4b72ec0cff
wph95 <2732352+wph95@users.noreply.github.com>
2024-08-17 20:37:58 -0700

    add __main__

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from indexer.symbol_indexer import SymbolIndexer
 from searcher.searcher import SymbolSearcher
 
 # SymbolIndexer.from_dir("/Users/justwph/labs/hackathons/2024/libs/django").index()
+searcher = SymbolSearcher("symbol_index.parquet")
 
-tokens = SymbolSearcher("symbol_index.parquet").tokens()
-for i in tokens:
-    print(i)
+
+results = searcher.search(["AddConstraintNotValid"])
+for r in results:
+    print(r)
